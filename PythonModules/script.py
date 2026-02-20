61d89346c080bfe65eba67026085c49b43271970
Disego Maile <disegonoah@gmail.com>
2026-02-20 15:00:27 +0200

    Modules

---
+++ PythonModules/script.py
@@ -0,0 +1,35 @@
+from math import sqrt, pow
+import math
+import math as m
+from time import time
+print(m.pi)
+
+
+import sys
+print(sys.path)
+
+import random
+random.randint(1,6)
+
+
+
+print (sqrt(16))
+print (pow(2,3))
+
+print(sqrt(25))
+print(pow(2,3))
+
+math.sqrt(16)
+math.factorial(5)
+
+math.sqrt(16)
+math.factorial(5)
+
+import os
+os.getcwd()
+
+from mymath import add, subtract
+
+print(add(5,3))
+print(subtract(5,3))
+
